33f59cc112ee4840cd7f712d2fdc6fc69d149388
OscarHuesos <34663748+OscarHuesos@users.noreply.github.com>
2019-09-06 21:33:56 -0500

    Add files via upload

---
+++ example1/main.cpp
@@ -0,0 +1,14 @@
+
+#include "prueba.h"
+
+
+using namespace std;
+int main(){
+printf("Hello world\n");
+Clase s;
+int a=6;
+int b=7;
+printf("MIs numeros son 6 7 y los sumare \n");
+s.suma(a,b);
+return 0;
+}
---
+++ example1/prueba.h
@@ -0,0 +1,28 @@
+#include <stdlib.h>	// standard C library function
+#include <cmath>	// math
+#include <fstream>	// file i/o
+#include <iostream>	// i/o stream
+#include <strstream>	// char *stream
+#include <iomanip>	// i/o format
+#include <string>  	// Standard string library
+#include <map>   	// STL associative container
+#include <limits.h>   	// Noeric limit
+#include <algorithm> 	// STL algorithm
+#include <vector>    	// STL vector
+#include <functional>	// STL functional
+#include <cmath>
+#include <stdio.h>
+#include <string.h>
+
+
+using namespace std;
+
+class Clase{
+
+public:
+void suma(int a, int b) {
+  int c=a+b;
+	printf("suma es : %i \n",c );
+}
+
+};
